91660bbe01e0439d2ac81162dcd80460e70bf9e8
alexagherlone <65496718+alexagherlone@users.noreply.github.com>
2020-06-26 21:51:33 -0400

    Added PNG File Sender Code

--- Selenium.py
+++ Selenium.py
@@ -13,82 +13,55 @@ driver = webdriver.Chrome(CHROMEDRIVER_PATH)
 
 driver.get("https://www.bls.gov/news.release/empsit.nr0.htm")
 print(driver.title) #> BLS Employment Situation
-driver.save_screenshot("unemployment_rate.pdf")
+driver.save_screenshot("unemployment_rate.png")
 
 #Generates Email Using SendGrid (Option 1)
 import os
-import base64
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
 
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 MY_EMAIL = os.environ.get("MY_EMAIL_ADDRESS")
+                                                                                                                                                                
+# Citing the Code Author: https://github.com/sendgrid/sendgrid-python/issues/340
 
-message = Mail(
-    from_email='MY_EMAIL_ADDRESS',
-    to_emails='MY_EMAIL_ADDRESS',
-    subject='Unemployment Data',
-    html_content='<Updated U.S Unemployment Rate Attached>'
-)
-
-with open('unemployment_rate.pdf', 'rb') as f:
-    data = f.read()
-    f.close()
-encoded_file = base64.b64encode(data).decode()
-
-attachedFile = Attachment(
-    FileContent(encoded_file),
-    FileName('unemployment_rate.pdf'),
-    FileType('application/pdf'),
-    Disposition('attachment')
-)
-message.attachment = attachedFile
-
-sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-response = sg.send(message)
-print(response.status_code, response.body, response.headers)
-
-                                                                                                                                                                              
-#Generates the email using SendGrid (Option 2)
-
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-MY_EMAIL = os.environ.get("MY_EMAIL_ADDRESS")
-
-def send_email(subject="Unemployment Data", html="<p>Unemployment Data</p>", pdf="unemployment_rate.pdf"):
+def send_email(subject="Unemployment Data", html="<p>Unemployment Data</p>", png="unemployment_rate.png"):
     client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
     message = Mail(from_email=MY_EMAIL, to_emails=MY_EMAIL, subject=subject, html_content=html)
-    #attaches the PDF we generated earlier
-    #file_path = 'unemployment_rate.png'
-    with open(file_path, 'rb') as f:
-       data = f.read()
-       f.close()
-    encoded = base64.b64encode(data).decode()
+    
+    #attaches the PNG we generated earlier
+    image_name = 'unemployment_rate.png'
+    with open(image_name, 'rb') as f:
+        image = f.read()
+        f.close()
+
+    image_encoded = base64.b64encode(image)
+
     attachment = Attachment()
-    attachment.file_content = FileContent(encoded)
-    attachment.file_type = FileType('application/pdf')
-    attachment.file_name = FileName('unemployment_rate.pdf')
-    attachment.disposition = Disposition('attachment')
+    attachment.content = image_encoded
+    attachment.type = "image/png"
+    attachment.filename = image_name
+    attachment.disposition = 'attachment'
     attachment.content_id = ContentId('Example Content ID')
     message.attachment = attachment
-    #send email
+    
+    #Send Email
     try:
         response = client.send(message)
         return response
     except Exception as e:
         print("OOPS", e.message)
         return None
-
-#where we get into the actual contents of the email, the message, subject, etc.
-if __name__ == "__main__":
-    email_subject = "Unemployment Data"
-    email_html = f""" 
-    <h3> Good Morning Alexa, the current unemployment rate in the U.S is attached</h3>
-    """
-    email_pdf = "unemployment_rate.pdf"
-    #send my message to users
-    send_email(email_subject, email_html, email_pdf)
-    print(" ")
-    print("Your email has been sent!")
-    print(" ")
     
+    #Email Contents
+
+    if __name__ == "__main__":
+        email_subject = "Unemployment Data"
+        email_html = f""" 
+        <p> Unemployment Data </p>
+        """
+        email_png = "unemployment_rate.png"
+   
+        #send my message to users
+        send_email(subject="Unemployment Data", html="<p>Unemployment Data</p>", png="unemployment_rate.png")
